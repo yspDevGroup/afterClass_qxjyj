a6e013203e95ac3f9bd5c71692209df6571c6c67
SissleLynly <861271413@qq.com>
2021-09-10 18:14:15 +0800

    增加企业微信对接

--- src/components/RightContent/AvatarDropdown.tsx
+++ src/components/RightContent/AvatarDropdown.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Spin } from 'antd';
 import { useAccess, useModel } from 'umi';
 import styles from './index.less';
 import { JYJGSJ } from '@/services/after-class-qxjyj/jyjgsj';
+import { initWXAgentConfig, initWXConfig, showUserName } from '@/wx';
 
 export type GlobalHeaderRightProps = {
   menu?: boolean;
@@ -13,6 +14,7 @@ const AvatarDropdown: React.FC<GlobalHeaderRightProps> = () => {
   const { initialState } = useModel('@@initialState');
   const { currentUser } = initialState || {};
   const [jyjData, setJyjData] = useState<any>();
+  const userRef = useRef(null);
   useEffect(() => {
     async function fetchData(jyjId: string) {
       const res = await JYJGSJ({
@@ -26,6 +28,21 @@ const AvatarDropdown: React.FC<GlobalHeaderRightProps> = () => {
       fetchData(currentUser?.jyjId);
     }
   }, [currentUser]);
+    useEffect(() => {
+    (async () => {
+      if (/MicroMessenger/i.test(navigator.userAgent)) {
+        await initWXConfig(['checkJsApi']);
+      }
+      if (await initWXAgentConfig(['checkJsApi'])) {
+        showUserName(userRef?.current, currentUser?.userId);
+        // 注意: 只有 agentConfig 成功回调后，WWOpenData 才会注入到 window 对象上面
+        WWOpenData.bindAll(document.querySelectorAll('ww-open-data'));
+      } else {
+        console.warn('微信登录过期，请重新授权');
+        message.warn('微信登录过期，请重新授权');
+      }
+    })();
+  }, []);
   const loading = (
     <span className={`${styles.action} ${styles.account}`}>
       <Spin
@@ -46,7 +63,7 @@ const AvatarDropdown: React.FC<GlobalHeaderRightProps> = () => {
         {jyjData ? <span style={{paddingRight:'40px'}}>
           {jyjData?.BMMC}
         </span> : ''}
-        <span className={`${styles.name} anticon`}>
+        <span className={`${styles.name} anticon`} ref={userRef}>
           {currentUser?.username}
           {isAdmin ? '' : '老师'}
         </span>
