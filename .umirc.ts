3ee88c03567a72beb3874aff3986b966dbbe5189
zhupengliang <zpl_dongxie@sina.com>
2021-09-17 09:41:40 +0800

    分开管理生产环境与测试环境配置

--- .umirc.ts
+++ .umirc.ts
@@ -12,13 +12,12 @@ export default defineConfig({
     ENV_title: '课后服务平台',
     ENV_subTitle: '课后服务平台',
     ENV_copyRight: '2021 版权所有：陕西五育汇智信息技术有限公司',
-    ENV_host: 'http://afterclassQxjyj.test.xianyunshipei.com',
-    ENV_backUrl: 'http://api.test.xianyunshipei.com',
-    ssoHost: 'http://platform.test.xianyunshipei.com',
+    ENV_host: 'http://afterclassQxjyj.prod.xianyunshipei.com',
+    ENV_backUrl: 'http://api.prod.xianyunshipei.com',
+    ssoHost: 'http://platform.prod.xianyunshipei.com',
     authType: 'wechat',
-    clientId: 'ww2b4b964ba635948b',
-    clientSecret: 'BwDHyfEiuBjdz18aR6Int96FxGZQ2d_UeJcVSBnkGvU'
-    // ENV_backUrl: 'http://192.168.0.113:3000'
+    clientId: 'wwccc22183061ae39b',
+    clientSecret: 'fyIAGkGZzBdoYun_Oka0NsGZqTmcovFTMMorCFrjRyg'
   },
   links: [{ rel: 'icon', href: './title.png' }],
   dynamicImport: {
@@ -27,7 +26,7 @@ export default defineConfig({
   theme,
   proxy: {
     '/api': {
-      target: 'http://api.test.xianyunshipei.com',
+      target: 'http://api.prod.xianyunshipei.com',
       changeOrigin: true,
       pathRewrite: { '^/api': '' }
     }
