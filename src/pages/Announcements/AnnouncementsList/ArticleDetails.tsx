e863a9f8294ec85e32d9a3e8e0ecb5c26f5d811e
WuXing <1205118129@qq.com>
2021-10-14 14:38:52 +0800

    通知公告详情无内容问题

--- src/pages/Announcements/AnnouncementsList/ArticleDetails.tsx
+++ src/pages/Announcements/AnnouncementsList/ArticleDetails.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '../index.module.less';
 import { history } from 'umi';
 import { LeftOutlined } from '@ant-design/icons';
 import { Button } from 'antd';
+import { JYJGTZGG } from '@/services/after-class-qxjyj/jyjgtzgg';
 
 const ArticleDetails = (props: any) => {
   const { state } = props.history.location;
+  console.log('state: ', state);
+  const [nrInfo, setNrInfo] = useState<any>();
+
+  useEffect(()=>{
+    getData();
+  },[state.id])
+
+  const getData = async () => {
+    const result = await JYJGTZGG({ id: state.id });
+
+    console.log('result: ', result);
+    setNrInfo(result.data.NR);
+  }
+
   return (
     <>
       <Button
@@ -23,7 +38,7 @@ const ArticleDetails = (props: any) => {
       <div className={styles.ArticleDetails}>
         <h1>{state.BT}</h1>
         <p className={styles.RQ}>时间：{state.RQ}</p>
-        <div dangerouslySetInnerHTML={{ __html: state.NR }} />
+        <div dangerouslySetInnerHTML={{ __html: nrInfo }} />
         <p className={styles.LY}>来源：{state.LY}</p>
       </div>
     </>
