1b967bee1f818bb4c72f9e50da91305eb03c4678
zhupengliang <zpl_dongxie@sina.com>
2021-10-14 22:04:41 +0800

    移动端登录后自动跳转到移动页面

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import { Access, useAccess, useModel } from 'umi';
+import { Access, useAccess, useModel, history } from 'umi';
 import { Carousel } from 'antd';
 import IndexComp from '@/components/IndexComp';
 import Register from '@/components/IndexComp/Register';
+import { envjudge } from '@/utils';
 
 const Index = () => {
   const { initialState } = useModel('@@initialState');
   const { currentUser } = initialState || {};
-  return <>{currentUser?.XZQHM ? <IndexComp /> : <Register />}</>;
+  if (!currentUser?.XZQHM) {
+    return <Register />;
+  }
+  const ej = envjudge();
+  if (ej === 'mobile' || ej === 'wx-mobile' || ej === 'com-wx-mobile') {
+    history.replace('/mobile/homepage')
+    return '';
+  }
+  return <IndexComp />;
 };
 Index.title = ENV_title;
 Index.access = '管理员';
