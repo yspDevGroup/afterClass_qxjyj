dbcd6d4b8b67e3d0de873d2cf1531a8275c6904c
SissleLynly <861271413@qq.com>
2021-10-22 11:48:12 +0800

    修改间距宽度

--- src/pages/Statistics/Graphic/component/utils.ts
+++ src/pages/Statistics/Graphic/component/utils.ts
@@ -2,9 +2,12 @@
  * @description:
  * @author: Sissle Lynn
  * @Date: 2021-08-24 19:59:45
- * @LastEditTime: 2021-09-29 19:49:49
+ * @LastEditTime: 2021-10-15 15:29:00
  * @LastEditors: Sissle Lynn
  */
+import should from '@/assets/should.png';
+import real from '@/assets/real.png';
+import leave from '@/assets/leave.png';
 export const textColor1 = ['#FF715C', '#60DD95', '#4FC4E7', '#FFA729'];
 export const textColor2 = ['#FFA729', '#60DD95', '#FF715C'];
 
@@ -81,6 +84,7 @@ export const pieConfig: any = {
 export const barConfig: any = {
   yField: 'school',
   xField: 'value',
+  appendPadding: 10,
   yAxis: {
     label: {
       autoRotate: false,
@@ -91,6 +95,10 @@ export const barConfig: any = {
     }
   },
   maxBarWidth: 30,
+  meta: {
+    school: { alias: '学校名称' },
+    value: { alias: '报名人数' },
+  },
 };
 export const defaultData: any = {
   serviceNum: [],
@@ -118,3 +126,207 @@ export const getTerm = () => {
     }
   }
 }
+export const mock: any = {
+  serviceNum: [{
+    title: '教师总数',
+    num: '45236'
+  },
+  {
+    title: '学生总数',
+    num: '45736'
+  },
+  {
+    title: '收款总额',
+    num: '70789'
+  },
+  {
+    title: '退款总额',
+    num: '4455'
+  }],
+  courseCollect: [{
+    type: '艺术类',
+    value: 34
+  }, {
+    type: '人文社科类',
+    value: 54
+  }, {
+    type: '体育类',
+    value: 23
+  }, {
+    type: '自然科学类',
+    value: 31
+  }, {
+    type: '社会实践类',
+    value: 13
+  }, {
+    type: '托管服务',
+    value: 37
+  }, {
+    type: '其他',
+    value: 12
+  }],
+  checkOut: [{
+    icon: should,
+    title: '应到人数',
+    num: 2100
+  }, {
+    icon: real,
+    title: '实到人数',
+    num: 1842
+  }, {
+    icon: leave,
+    title: '请假人数',
+    num: 258
+  }],
+  numCollect: [{
+    title: '学校总数',
+    num: 21
+  }, {
+    title: '机构总数',
+    num: 28
+  }, {
+    title: '课程总数',
+    num: 123
+  }, {
+    title: '课程班总数',
+    num: 234
+  }, {
+    title: '学校教师总数',
+    num: 45
+  }, {
+    title: '机构教师总数',
+    num: 23
+  }],
+  schoolNum: [
+    '西安市高新区第七高级中学',
+    '西安高新区第五高级中学',
+    '西安高新区第四高级中学',
+    '西安高新一中分校(高新区第三学校)',
+    '西安市鄠邑区秦风初级中学',
+    '西北大学附属中学',
+    '西安高新区第一高级中学',
+    '西安市高新区第二高级中学',
+    '西安市高新区第三高级中学',
+    '西安市高新区第六高级中学'
+  ],
+  courseNum: [
+    {
+      type: '本校课程',
+      school: '西安市高新区第七高级中学',
+      value: 35,
+    },
+    {
+      type: '本校课程',
+      school: '西安高新区第五高级中学',
+      value: 39,
+    },
+    {
+      type: '本校课程',
+      school: '西安高新区第四高级中学',
+      value: 47,
+    },
+    {
+      type: '本校课程',
+      school: '西安高新一中分校(高新区第三学校)',
+      value: 42,
+    },
+    {
+      type: '本校课程',
+      school: '西安市鄠邑区秦风初级中学',
+      value: 34,
+    },
+    {
+      type: '本校课程',
+      school: '西北大学附属中学',
+      value: 28,
+    },
+    {
+      type: '机构课程',
+      school: '西安市高新区第七高级中学',
+      value: 23,
+    },
+    {
+      type: '机构课程',
+      school: '西安高新区第五高级中学',
+      value: 26,
+    },
+    {
+      type: '机构课程',
+      school: '西安高新区第四高级中学',
+      value: 38,
+    },
+    {
+      type: '机构课程',
+      school: '西安高新一中分校(高新区第三学校)',
+      value: 17,
+    },
+    {
+      type: '机构课程',
+      school: '西安市鄠邑区秦风初级中学',
+      value: 29,
+    },
+    {
+      type: '机构课程',
+      school: '西北大学附属中学',
+      value: 28,
+    },
+  ],
+  enrollNum: [
+    {
+      school: '西安市高新区第七高级中学',
+      value: 35,
+    },
+    {
+      school: '西安高新区第五高级中学',
+      value: 39,
+    },
+    {
+      school: '西安高新区第四高级中学',
+      value: 47,
+    },
+    {
+      school: '西安高新一中分校(高新区第三学校)',
+      value: 42,
+    },
+    {
+      school: '西安市高新区第三高级中学',
+      value: 28,
+    },
+    {
+      school: '西安市鄠邑区秦风初级中学',
+      value: 34,
+    },
+    {
+      school: '西北大学附属中学',
+      value: 28,
+    },
+    {
+      school: '西安市高新区第七高级中学',
+      value: 23,
+    },
+    {
+      school: '西安市高新区第六高级中学',
+      value: 34,
+    },
+    {
+      school: '西安市高新区第八高级中学',
+      value: 21,
+    },
+    {
+      school: '西安市高新区第九高级中学',
+      value: 23,
+    },
+  ],
+  agentNum: [
+    '贝贝安亲课后服务中心',
+    '晋级课后服务中心',
+    '小新星课后服务中心',
+    '允善课后托育',
+    '小雨滴课后服务中心',
+    '顺利小饭桌课后管理中心',
+    '阳光课后管理中心',
+    '响叮当课后成长中心',
+    '锦程课后教育',
+    '未来课后托管'
+  ]
+};
