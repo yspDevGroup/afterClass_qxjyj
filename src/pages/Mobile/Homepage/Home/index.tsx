ad15a7ced7cba919970b6de0b5e43f1c74301a06
zhupengliang <zpl_dongxie@sina.com>
2021-11-01 15:51:26 +0800

    非微信认证情况下退出时转到首页

--- src/pages/Mobile/Homepage/Home/index.tsx
+++ src/pages/Mobile/Homepage/Home/index.tsx
@@ -33,7 +33,7 @@ const Home = () => {
             <a onClick={() => {
               setInitialState({ ...initialState!, currentUser: null });
               removeOAuthToken();
-              history.replace('/authCallback/overDue');
+              history.replace(initialState?.buildOptions.authType === 'wechat' ? '/authCallback/overDue' : '/');
             }}>
               <IconFont type="icon-tuichu" className={styles.signOut} />
             </a>
