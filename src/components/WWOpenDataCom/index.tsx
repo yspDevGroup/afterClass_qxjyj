8994b72e3eebe32576e9a4c09f839eb0e3e294bc
zhupengliang <zpl_dongxie@sina.com>
2021-11-01 15:51:48 +0800

    微信名称转义失败时，显示用户ID

--- src/components/WWOpenDataCom/index.tsx
+++ src/components/WWOpenDataCom/index.tsx
@@ -1,15 +1,37 @@
-import React, { useRef, useLayoutEffect } from 'react';
+import React, { useRef, useLayoutEffect, useState, CSSProperties } from 'react';
 
 type PropsType = {
   type: 'userName' | 'departmentName';
   openid: string;
+  style?: CSSProperties;
 };
-export default function WWOpenDataCom({ type, openid }: PropsType) {
+export default function WWOpenDataCom({ type, openid, style = {} }: PropsType) {
   const ref = useRef(null);
+  const [showWechat, setShowWechat] = useState(true);
+
   useLayoutEffect(() => {
-    if (openid && typeof WWOpenData !== 'undefined' && WWOpenData?.bind) {
+    if (openid && typeof WWOpenData !== 'undefined' && WWOpenData.bind) {
       WWOpenData.bind(ref.current);
+    } else {
+      setShowWechat(false);
     }
   }, [openid]);
-  return <ww-open-data ref={ref} type={type} openid={openid} />;
+  if (showWechat) {
+    return <ww-open-data style={{ color: '#333', ...style }} ref={ref} type={type} openid={openid} />;
+  }
+  return (
+    <span
+      style={{
+        color: '#333',
+        display: 'inline-block',
+        maxWidth: '5em',
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+        whiteSpace: 'nowrap',
+        ...style
+      }}
+    >
+      {openid}
+    </span>
+  );
 }
